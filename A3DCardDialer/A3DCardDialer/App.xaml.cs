acaaf36b47ba4b964044e97972d26d0f9c61d2b8
Dhananjay Kr Pandey <dhananjaykrpandey@outlook.com>
2020-03-19 16:47:15 +0400

    first test

--- A3DCardDialer/A3DCardDialer/App.xaml.cs
+++ A3DCardDialer/A3DCardDialer/App.xaml.cs
@@ -10,7 +10,8 @@ namespace A3DCardDialer
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new TbMainPage());
+                       
         }
 
         protected override void OnStart()
---
+++ A3DCardDialer/A3DCardDialer/TbMainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace A3DCardDialer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TbMainPage : TabbedPage
+    {
+        public TbMainPage()
+        {
+            InitializeComponent();
+            this.Title = "A3D Card Dialer";
+            this.IconImageSource = ImageSource.FromResource("A3D.ico");
+            
+        }
+    }
+}
